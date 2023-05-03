9138bb060d65ba178fae649fb9bcdc5ba08b6a2f
seaflower <mikeqoo123@gmail.com>
2023-05-03 10:54:45 +0800

    增加圖檔

--- data/file.sql
+++ data/file.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `nici` (
     blood varchar(10) NOT NULL COMMENT '血型',
     starsign varchar(20) NOT NULL COMMENT '星座',
     series varchar(50) NOT NULL COMMENT '系列',
+    img varchar(50) NOT NULL COMMENT '圖檔名稱',
     PRIMARY KEY(`id`, `name`)
 ) COMMENT='Nici的身份詳細表';
 
