84d2f9e640b795a6053b52e0d1dfc58f42063eed
seaflower <mikeqoo123@gmail.com>
2023-05-08 10:36:35 +0800

    增加log機制

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gorm.io/gorm"
 )
@@ -16,6 +19,65 @@ var (
 	conn    *gorm.DB
 	niciobj []nici.Nici
 )
+
+func Logger() *logrus.Logger {
+	now := time.Now()
+	logFilePath := ""
+	if dir, err := os.Getwd(); err == nil {
+		logFilePath = dir + "/logs/"
+	}
+	if err := os.MkdirAll(logFilePath, 0777); err != nil {
+		fmt.Println(err.Error())
+	}
+	logFileName := now.Format("2006-01-02") + ".log"
+	//log文件
+	fileName := path.Join(logFilePath, logFileName)
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	//寫入文件
+	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	logger := logrus.New()
+	logger.Out = src
+	//設log等級
+	logger.SetLevel(logrus.DebugLevel)
+	//log格式
+	logger.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	return logger
+}
+
+func LoggerToFile() gin.HandlerFunc {
+	logger := Logger()
+	return func(c *gin.Context) {
+		startTime := time.Now()
+		// 處理請求
+		c.Next()
+		endTime := time.Now()
+		// 執行時間
+		latencyTime := endTime.Sub(startTime)
+		// 請求方式
+		reqMethod := c.Request.Method
+		reqUri := c.Request.RequestURI
+		statusCode := c.Writer.Status()
+		clientIP := c.ClientIP()
+		//log格式
+		logger.Infof("| %3d | %13v | %15s | %s | %s |",
+			statusCode,
+			latencyTime,
+			clientIP,
+			reqMethod,
+			reqUri,
+		)
+	}
+}
 
 func init() {
 	var errdb error
@@ -86,18 +148,16 @@ func conform(c *gin.Context) {
 	var sqlstr string
 	var results *gorm.DB
 	if blood == "" || len(blood) == 0 {
-		fmt.Println("進入星座", star)
 		sqlstr = "starsign = ?"
 		results = conn.Where(sqlstr, star).Find(&niciobj)
 	} else if star == "" || len(star) == 0 {
-		fmt.Println("進入血型", blood)
 		sqlstr = "blood = ?"
 		results = conn.Where(sqlstr, blood).Find(&niciobj)
 	} else {
-		fmt.Println("都進", star, blood)
 		sqlstr = "starsign = ? AND blood = ?"
 		results = conn.Where(sqlstr, star, blood).Find(&niciobj)
 	}
+	Logger().Info("輸入的條件", star, blood)
 	// c.JSON(http.StatusOK, gin.H{
 	// 	"blood":   blood,
 	// 	"star":    star,
@@ -141,6 +201,7 @@ func main() {
 	addr := fmt.Sprintf("%s:%d", host, port)
 	router := gin.Default()
 	router.Use(crosHandler())
+	router.Use(LoggerToFile())
 
 	// 設置模板路徑
 	router.LoadHTMLGlob("templates/*.html")
