085b4be6f88e86a7c8bbbf5c976c98e6ba6f3c9a
seaflower <mikeqoo123@gmail.com>
2023-05-09 09:54:34 +0800

    add sql log

--- lib/tidb/tidb.go
+++ lib/tidb/tidb.go
@@ -3,13 +3,16 @@ package tidb
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
+	"path"
 	"time"
 
 	viper "github.com/spf13/viper"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 const (
@@ -57,9 +60,40 @@ func (tidb *TiDB) GetDB() (*gorm.DB, error) {
 	var db *sql.DB
 	//組合sql連線字串
 	addr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", tidb.User, tidb.Passwd, tidb.Ip, tidb.Port, tidb.Database)
-
+	//產生sql log
+	now := time.Now()
+	logFilePath := ""
+	if dir, err := os.Getwd(); err == nil {
+		logFilePath = dir + "/logs/"
+	}
+	if err := os.MkdirAll(logFilePath, 0777); err != nil {
+		fmt.Println(err.Error())
+	}
+	logFileName := now.Format("2006-01-02") + "_sql.log"
+	//log文件
+	fileName := path.Join(logFilePath, logFileName)
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	//寫入文件
+	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	newLogger := logger.New(
+		log.New(src, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  false,       // Disable color
+		},
+	)
 	// 先開啟
-	conn, err = gorm.Open(mysql.Open(addr), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	conn, err = gorm.Open(mysql.Open(addr), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		return nil, err
 	}
