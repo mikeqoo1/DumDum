6a892f3cb8539a26dbf757a90833b9c965d45c64
seaflower <mikeqoo123@gmail.com>
2023-05-30 09:13:47 +0800

    增加腦包資料

--- data/file.sql
+++ data/file.sql
@@ -8,3 +8,11 @@ CREATE TABLE IF NOT EXISTS `nici` (
     PRIMARY KEY(`id`, `name`)
 ) COMMENT='Nici的身份詳細表';
 
+CREATE TABLE IF NOT EXISTS `userlist` (
+    id int(11) AUTO_INCREMENT NOT NULL COMMENT '流水編號',
+    account varchar(50) NOT NULL COMMENT '使用者帳號',
+    username varchar(50) NOT NULL COMMENT '使用者名稱',
+    status int(2) NOT NULL DEFAULT 1 COMMENT '狀態 0:關閉 1:啟用',
+    PRIMARY KEY(`id`, `account`)
+) COMMENT='使用者清單';
+
---
+++ data/shuming.sql
@@ -0,0 +1,3 @@
+INSERT INTO sea.userlist (account,username,status) VALUES
+	 ('test1','測試人員1',1),
+	 ('test2','測試人員2',1);
